c745bc6dacb77b13267ca95e0960b74d1dd70fbb
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-07-01 16:09:21 +0900

    flutter 예시 project 생성

---
+++ flutter/riverpod/dart_class_generator/dart_class/android/app/src/main/kotlin/com/example/dart_class/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_class
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter/riverpod/dart_class_generator/dart_class/lib/models/person.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Person {
+  final int id;
+  final String name;
+  final String? email;
+ 
+  Person({
+    required this.id,
+    required this.name,
+    this.email,
+  });
+
+  @override
+  String toString() => 'Person(id: $id, name: $name, email: $email)';
+
+  Person copyWith({
+    int? id,
+    String? name,
+    String? email,
+  }) {
+    return Person(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+    );
+  }
+}
